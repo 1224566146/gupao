dc71bbcc2390e5082d6d464299752256cb7010c5
樊高风 <13264426426@163.com>
2020-05-19 16:28:33 +0800

    提交建造者模式

---
+++ sheji-demo/src/main/java/com/fan/gupao/pattern/builder/simple/Course.java
@@ -0,0 +1,66 @@
+package com.fan.gupao.pattern.builder.simple;
+
+/**
+ * @Description:
+ * @Author: 樊高风
+ * @Date: 14:07 2020/5/19
+ */
+public class Course {
+
+    private String name;
+    private String ppt;
+    private String video;
+    private String note;
+    private String homeWork;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPpt() {
+        return ppt;
+    }
+
+    public void setPpt(String ppt) {
+        this.ppt = ppt;
+    }
+
+    public String getVideo() {
+        return video;
+    }
+
+    public void setVideo(String video) {
+        this.video = video;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getHomeWork() {
+        return homeWork;
+    }
+
+    public void setHomeWork(String homeWork) {
+        this.homeWork = homeWork;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "name='" + name + '\'' +
+                ", ppt='" + ppt + '\'' +
+                ", video='" + video + '\'' +
+                ", note='" + note + '\'' +
+                ", homeWork='" + homeWork + '\'' +
+                '}';
+    }
+}
