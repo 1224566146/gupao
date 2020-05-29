839086a91fc0b7e1b6b2909c0a20799e60d20849
樊高风 <13264426426@163.com>
2020-05-29 20:17:49 +0800

    提交springboot整合dubbo之provider

---
+++ springboot-provider/sample-api/src/main/java/com/fan/springboot/dubbo/demo/IHelloService.java
@@ -0,0 +1,11 @@
+package com.fan.springboot.dubbo.demo;
+
+/**
+ * @Description:
+ * @Author: 樊高风
+ * @Date: 17:16 2020/5/29
+ */
+public interface IHelloService {
+
+    String sayHello(String name);
+}
