33e38749f52e01d72fd84792d3bff2570459c23b
樊高风 <13264426426@163.com>
2020-05-29 20:18:31 +0800

    提交springboot整合dubbo之consumer

---
+++ springboot-consumer/src/test/java/com/fan/springboot/dubbo/demo/springbootconsumer/SpringbootConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fan.springboot.dubbo.demo.springbootconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
