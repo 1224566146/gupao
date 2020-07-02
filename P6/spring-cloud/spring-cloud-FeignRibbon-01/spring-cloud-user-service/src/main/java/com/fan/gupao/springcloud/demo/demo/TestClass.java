966458ee3227bf54357c30f94a083b74e9477c86
樊高风 <13264426426@163.com>
2020-07-02 11:35:12 +0800

    提交Spring-cloud的ribbon客户端

---
+++ P6/spring-cloud/spring-cloud-FeignRibbon-01/spring-cloud-user-service/src/main/java/com/fan/gupao/springcloud/demo/demo/TestClass.java
@@ -0,0 +1,22 @@
+package com.fan.gupao.springcloud.demo.demo;
+
+/**
+ * @author 樊高风
+ * @date 2020/7/2
+ */
+public class TestClass {
+
+    private String name;
+
+    public TestClass(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
