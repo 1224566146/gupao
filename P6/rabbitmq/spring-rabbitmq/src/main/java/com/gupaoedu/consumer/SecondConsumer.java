08053ef96001baf1317dadc2a845b5b4821720ef
樊高风 <13264426426@163.com>
2020-10-26 15:06:53 +0800

    提交Spring RabbitMQ

---
+++ P6/rabbitmq/spring-rabbitmq/src/main/java/com/gupaoedu/consumer/SecondConsumer.java
@@ -0,0 +1,18 @@
+package com.gupaoedu.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageListener;
+
+/**
+ *
+ */
+public class SecondConsumer implements MessageListener {
+
+    private Logger logger = LoggerFactory.getLogger(SecondConsumer.class);
+
+    public void onMessage(Message message) {
+        logger.info("The second consumer received message:"+new String(message.getBody()));
+    }
+}
